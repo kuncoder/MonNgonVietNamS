2703cffb286e80f297f18852d5a3f8021af0b731
PHẠM VIẾT THẮNG <phamvietthang>
2017-11-13 11:43:40 -0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/monngonviet/LikeActivity.java
@@ -0,0 +1,14 @@
+package com.example.dell.monngonviet;
+
+import android.os.Bundle;
+
+/**
+ * Created by KA on 11/10/2017.
+ */
+
+public class LikeActivity extends MainActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_like);
+    }
+}
